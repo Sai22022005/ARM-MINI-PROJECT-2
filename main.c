850d0db4ef61a90bcefcc6d523978e22ec518442
Edula Sai Kumar Reddy <edulasaikumarreddy@gmail.com>
2026-07-20 16:59:03 +0530

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 	#define SW4 (1<<21)   // LOGOUT
 	
 	int main(void) 
-		{
+	{
 			uart0_init(9600);
 			lcd_init();
 			lcd_string("AI USER MONITOR");
@@ -27,39 +27,39 @@ while(1)
 {
     char bt_cmd = 0;
     if (U0LSR & 0x01)
-			{                  // Check if Bluetooth sent data
-        bt_cmd = U0RBR;
-			}
+		{                  // Check if Bluetooth sent data
+        	bt_cmd = U0RBR;
+		}
 
     if (bt_cmd == '1' || !(IOPIN0 & SW1)) // LOGIN
-			{
+		{
         lcd_cmd(0xC0);
         lcd_string("LOGIN");
         uart0_string("User Logged In\r\n");
-			}
+		}
     else if (bt_cmd == '2' || !(IOPIN0 & SW2))  // ACTIVE
-			{
+		{
         lcd_cmd(0xC0);
         lcd_string("USER ACTIVE");
         IOCLR0 = GREEN_LED;   // Green LED ON (active-low)
         IOSET0 = RED_LED;     // Red LED OFF
         uart0_string("AI STATUS : ACTIVE\r\n");
-      }
+      	}
    
     else if (bt_cmd == '3' || !(IOPIN0 & SW3)) // STATUS
-			{
+		{
         lcd_cmd(0xC0);
         lcd_string("STATUS CHECK");
         uart0_string("Monitoring Status\r\n");
-			}
+		}
 			// LOGOUT
     else if (bt_cmd == '4' || !(IOPIN0 & SW4)) // LOGOUT
-			{
+		{
         lcd_cmd(0xC0);
         lcd_string("LOGOUT");
         IOCLR0 = RED_LED;     // Red LED ON
         IOSET0 = GREEN_LED;   // Green LED OFF
         uart0_string("WARNING : IDLE USER\r\n");
-			}
+		}
 	}
 }
